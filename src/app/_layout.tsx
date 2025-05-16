c769d946bda6033bc6707d0795a75fd245857278
Mark <markmccoid@gmail.com>
2025-05-15 23:41:11 -0400

    update theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,14 @@
 import '~/global.css';
 
-import { Slot, Stack, useRouter, useSegments } from 'expo-router';
+import { Slot } from 'expo-router';
 import { useEffect } from 'react';
-import { Appearance, View, useColorScheme } from 'react-native';
+import { Appearance, View } from 'react-native';
 import { AuthProvider, useAuth } from '~/authentication/AuthProvider';
 import * as SplashScreen from 'expo-splash-screen';
 import { initTMDB } from '@markmccoid/tmdb_api';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { CustomLightTheme, CustomDarkTheme } from '../utils/customColorTheme';
-// import { useColorScheme } from 'nativewind';
+import { useColorScheme } from 'nativewind';
 import { ThemeProvider } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Asul_400Regular, Asul_700Bold } from '@expo-google-fonts/asul';
@@ -17,17 +17,14 @@ import { useFonts } from '@expo-google-fonts/asul';
 import { setupEvents } from '~/utils/events';
 import { queryClient } from '~/utils/queryClient';
 import { useSyncQueries } from 'tanstack-query-dev-tools-expo-plugin';
-import { selectEligibleShows, registerBackgroundTask } from '~/utils/backgroundTasks';
+import { registerBackgroundTask } from '~/utils/backgroundTasks';
 import { use$ } from '@legendapp/state/react';
 import { initializeWatchProviders, settings$ } from '~/store/store-settings';
-import * as Notifications from 'expo-notifications';
 import { askNotificationPermissions } from '~/utils/permissions';
 import useNotificationObserver from '~/utils/notificationObserver';
 
 const InitialLayout = () => {
-  const router = useRouter();
   const { currentUser, initialized } = useAuth();
-  const segments = useSegments();
   const [fontsLoaded, error] = useFonts({ Asul_700Bold, Roboto_500Medium, Roboto_400Regular });
   //! Notification Observer.  Added because notification links were not
   //! getting through via expo-router defaults.
@@ -93,15 +90,15 @@ const InitialLayout = () => {
 };
 
 export default function RootLayout() {
-  // const { colorScheme } = useColorScheme();
-  const colorScheme = useColorScheme();
+  const { colorScheme } = useColorScheme();
   const defaultTheme = use$(settings$.defaultTheme);
 
-  const finalTheme = defaultTheme === 'auto' ? colorScheme : defaultTheme;
+  const finalTheme = defaultTheme === 'auto' ? (colorScheme ?? 'light') : defaultTheme;
 
   useEffect(() => {
     Appearance.setColorScheme(finalTheme);
   }, [finalTheme]);
+
   //! Uncomment to use the react query dev tools
   // useReactQueryDevTools(queryClient);
   // Appearance.setColorScheme('light');
